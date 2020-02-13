736dc1cbddb501bb306913670b2e521b5b210e83
Comeau <e1895410@cmaisonneuve.qc.ca>
2020-02-13 12:03:11 -0500

    modif du head (ajout google font et lien css)

--- app_photo/views/pages/head.php
+++ app_photo/views/pages/head.php
@@ -1,9 +1,23 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
     <head>
-        <title>Page liste des founisseurs/ADMIN</title>
-        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-    </head>
+        <meta charset="UTF-8">
+        <meta name="author" content='Salah, Kervens, Olivier, Gaël'>
+        <meta name='description' content='Appareils et services pour la photographie.'>
+        <meta name='keywords' content='photographie, réparation, scéance photo, appareil photo'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>picturing</title>
+        
+        <!-- Police de caractères -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,900&display=swap" rel="stylesheet">
+        <!-- Feuilles de style -->
+        <link rel="stylesheet" href=".assets/styles/stylesheets/normalize.css">
+        <link rel="stylesheet" href=".assets/styles/sass/main.css">
 
-    <body>
+        <!-- integration du script -->
+        <script type="module" src=".assets/scripts/main.js"></script>
+	</head>
+<body>
