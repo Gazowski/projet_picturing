8ccd2b766fae52cc7422c7350f337a4c396785b9
Comeau <e1895410@cmaisonneuve.qc.ca>
2020-02-13 11:42:20 -0500

    modif db (ajout de donnée)

--- fichiers_projet/database/db_picturing.sql
+++ fichiers_projet/database/db_picturing.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Feb 12, 2020 at 02:19 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.14
+-- Host: 127.0.0.1
+-- Generation Time: Feb 13, 2020 at 04:40 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,69 +23,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ad`
---
-
-DROP TABLE IF EXISTS `ad`;
-CREATE TABLE IF NOT EXISTS `ad` (
-  `id_ad` int(11) NOT NULL AUTO_INCREMENT,
-  `location` varchar(250) DEFAULT NULL,
-  `title` varchar(250) DEFAULT NULL,
-  `description` text,
-  `price` varchar(50) DEFAULT NULL,
-  `photo` varchar(250) NOT NULL,
-  `open_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `close_date` datetime NULL DEFAULT NULL,
-  `owner` int(11) NOT NULL,
-  PRIMARY KEY (`id_ad`),
-  UNIQUE KEY `title` (`title`),
-  KEY `owner` (`owner`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `banishement`
---
-
-DROP TABLE IF EXISTS `banishement`;
-CREATE TABLE IF NOT EXISTS `banishement` (
-  `id_banishement` int(11) NOT NULL AUTO_INCREMENT,
-  `banished_member` int(11) NOT NULL,
-  `admin_member` int(11) NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_banishement`),
-  KEY `banished_member` (`banished_member`),
-  KEY `admin_member` (`admin_member`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE IF NOT EXISTS `category` (
-  `id_category` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `ad` int(11) NOT NULL,
-  `created_by` int(11) NOT NULL,
-  PRIMARY KEY (`id_category`),
-  UNIQUE KEY `name` (`name`),
-  KEY `ad` (`ad`),
-  KEY `created_by` (`created_by`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `members`
 --
 
-DROP TABLE IF EXISTS `members`;
-CREATE TABLE IF NOT EXISTS `members` (
-  `id_members` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `members` (
+  `id_members` int(11) NOT NULL,
   `password` varchar(50) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
@@ -103,93 +43,47 @@ CREATE TABLE IF NOT EXISTS `members` (
   `status` varchar(50) NOT NULL,
   `inscription_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `validation_date` datetime NOT NULL,
-  `approved_by` int(11) NOT NULL,
-  PRIMARY KEY (`id_members`),
-  UNIQUE KEY `email` (`email`),
-  KEY `approved_by` (`approved_by`)
+  `approved_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `message`
+-- Dumping data for table `members`
 --
 
-DROP TABLE IF EXISTS `message`;
-CREATE TABLE IF NOT EXISTS `message` (
-  `id_message` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(250) NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `text_message` varchar(250) DEFAULT NULL,
-  `writer` int(11) NOT NULL,
-  `ad` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_message`),
-  KEY `writer` (`writer`),
-  KEY `ad` (`ad`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+INSERT INTO `members` (`id_members`, `password`, `first_name`, `last_name`, `entreprise_name`, `entreprise_number`, `address`, `tel`, `email`, `website_link`, `social_networks_link`, `role`, `provider_profil`, `status`, `inscription_date`, `validation_date`, `approved_by`) VALUES
+(1, 'admin', 'admin', 'admin', NULL, NULL, 'adresse de l\'admin', 1234, 'email@admin.com', NULL, NULL, 'admin', NULL, 'valid', '2020-02-13 09:52:28', '2020-02-13 09:52:28', 1),
+(2, '1234', 'salah', 'salhi', NULL, NULL, 'laval', 1234, 'salah@email.com', NULL, NULL, 'supplier', '1', 'valid', '2020-02-13 10:36:33', '2020-02-13 10:36:33', 1);
 
 --
--- Table structure for table `rating`
+-- Indexes for dumped tables
 --
 
-DROP TABLE IF EXISTS `rating`;
-CREATE TABLE IF NOT EXISTS `rating` (
-  `id_rating` int(11) NOT NULL AUTO_INCREMENT,
-  `rated_user` int(11) NOT NULL,
-  `rater_user` int(11) NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `rating` int(11) NOT NULL,
-  PRIMARY KEY (`id_rating`),
-  KEY `rated_user` (`rated_user`),
-  KEY `rater_user` (`rater_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id_members`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `approved_by` (`approved_by`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id_members` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `ad`
---
-ALTER TABLE `ad`
-  ADD CONSTRAINT `ad_ibfk_2` FOREIGN KEY (`owner`) REFERENCES `members` (`id_members`);
-
---
--- Constraints for table `banishement`
---
-ALTER TABLE `banishement`
-  ADD CONSTRAINT `banishement_ibfk_1` FOREIGN KEY (`banished_member`) REFERENCES `members` (`id_members`),
-  ADD CONSTRAINT `banishement_ibfk_2` FOREIGN KEY (`admin_member`) REFERENCES `members` (`id_members`);
-
---
--- Constraints for table `category`
---
-ALTER TABLE `category`
-  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`ad`) REFERENCES `ad` (`id_ad`),
-  ADD CONSTRAINT `category_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `members` (`id_members`);
 
 --
 -- Constraints for table `members`
 --
 ALTER TABLE `members`
   ADD CONSTRAINT `members_ibfk_2` FOREIGN KEY (`approved_by`) REFERENCES `members` (`id_members`);
-
---
--- Constraints for table `message`
---
-ALTER TABLE `message`
-  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`writer`) REFERENCES `members` (`id_members`),
-  ADD CONSTRAINT `message_ibfk_3` FOREIGN KEY (`ad`) REFERENCES `ad` (`id_ad`);
-
---
--- Constraints for table `rating`
---
-ALTER TABLE `rating`
-  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`rated_user`) REFERENCES `members` (`id_members`),
-  ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`rater_user`) REFERENCES `members` (`id_members`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
