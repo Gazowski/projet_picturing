bc1cdb898ca6e6e0bcffd80ec493ee1787854903
Comeau <e1895410@cmaisonneuve.qc.ca>
2020-02-20 14:44:17 -0500

    suppression mauvaise balise fermeture

--- app_photo/views/pages/create_ad_form.php
+++ app_photo/views/pages/create_ad_form.php
@@ -21,61 +21,60 @@
             <div id="infoMessage"><?php echo $message;?></div>
 
 
-      <?php echo form_open("create_ad");?>
+            <?php echo form_open("create_ad");?>
 
-            <p>
-                  <?php echo form_label($title['name']);?> <br />
-                  <?php echo form_input($title);?>
-            </p>
+                  <p>
+                        <?php echo form_label($title['name']);?> <br />
+                        <?php echo form_input($title);?>
+                  </p>
 
-<!--  ---------------------------------------------les selects------------------------------------------------------- -->
-            <div class="select_cont">
-                  
-                  <?php echo form_label($type['name']); ?>
-                  <div class="select">
-                  <?php 
-                        $opt = ['default' => '-- choisir un type --'];
-                        foreach($type['option'] as $key=>$option)
-                              {
-                                    $opt[$option->category] = $option->category;
-                              } 
-                        echo form_dropdown($type['name'], $opt,'', 'data-js-input="type"'); ?>
+      <!--  ---------------------------------------------les selects------------------------------------------------------- -->
+                  <div class="select_cont">                        
+                        <?php echo form_label($type['name']); ?>
+                        <div class="select">
+                              <?php 
+                                    $opt = ['default' => '-- choisir un type --'];
+                                    foreach($type['option'] as $key=>$option)
+                                          {
+                                                $opt[$option->category] = $option->category;
+                                          } 
+                                    echo form_dropdown($type['name'], $opt,'', 'data-js-input="type"'); 
+                              ?>
+                        </div>
                   </div>
-            </div>
 
-            <div class="select_cont">
-                  <?php echo form_label($category['name']);?> 
-                  <?php echo form_dropdown($category['name'],$category['option'],'','data-js-input="category"');?>
+                  <div class="select_cont">
+                        <?php echo form_label($category['name']);?> 
+                        <?php echo form_dropdown($category['name'],$category['option'],'','data-js-input="category"');?>
                   </div>
-            </div>
-<!--  -->
-            <p>
-                  <?php echo form_label($description['name']);?> <br />
-                  <?php echo form_input($description);?>
-            </p>
-      
 
-            <p>
-                  <?php echo form_label($price['name']);?> <br />
-                  <?php echo form_input($price);?>
-            </p>
+                  <p>
+                        <?php echo form_label($description['name']);?> <br />
+                        <?php echo form_input($description);?>
+                  </p>
+            
 
-            <p>
-                  <?php echo form_label($photo['name']);?> <br />
-                  <?php echo form_input($photo);?>
-            </p>
+                  <p>
+                        <?php echo form_label($price['name']);?> <br />
+                        <?php echo form_input($price);?>
+                  </p>
 
-            <p>
-                  <?php echo form_label($location['name']);?> <br />
-                  <?php echo form_input($location);?>
-            </p>  
+                  <p>
+                        <?php echo form_label($photo['name']);?> <br />
+                        <?php echo form_input($photo);?>
+                  </p>
+
+                  <p>
+                        <?php echo form_label($location['name']);?> <br />
+                        <?php echo form_input($location);?>
+                  </p>  
 
 
-            <div class="button_container">
-                  <?php echo form_submit('submit', 'ajouter');?>
-            </div>
+                  <div class="button_container">
+                        <?php echo form_submit('submit', 'ajouter');?>
+                  </div>
 
-      <?php echo form_close();?>
+            <?php echo form_close();?>
 
       </div>
 </div>
