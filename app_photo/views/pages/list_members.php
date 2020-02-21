7e3cd2a6b49d71546ab3d055ad883262bf4af4b5
Comeau <e1895410@cmaisonneuve.qc.ca>
2020-02-21 11:40:14 -0500

    list_member : ajout data-component et correction

--- app_photo/views/pages/list_members.php
+++ app_photo/views/pages/list_members.php
@@ -1,28 +1,25 @@
-<div'>
-    <div >
-        <table>
-            <tr class=''>
-
-                <th>Prénom</th>
-                <th>Nom</th>
-                <th>Courriel</th>
-                <th>Entprise</th>
-                <th>Groupe</th>
-                <th>Action</th>
-            </tr>
-            <?php foreach($membres as $membre)   
-                {    
-                    echo " <tr id=".$membre['id']."class =''>        
-                            <td>".$membre['first_name']."</td>
-                            <td>".$membre['last_name']." </td>
-                            <td>".$membre['email']."</td>
-                            <td>".$membre['company']."</td>
-                            <td>".$membre['name']."</td>
-                            <td><a href='' class='button'></a>                 
-                            </tr>";
-                }      
-            ?>
-        </table>
-    </div>
+<div >
+    <table data-component="list">
+        <tr class=''>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>Courriel</th>
+            <th>Entprise</th>
+            <th>Groupe</th>
+            <th>Action</th>
+        </tr>
+        <?php foreach($membres as $membre)   
+            {    
+                echo "<tr id=".$membre['id']."class ='' data-active=".$membre['active'].">        
+                        <td>".$membre['first_name']."</td>
+                        <td>".$membre['last_name']." </td>
+                        <td>".$membre['email']."</td>
+                        <td>".$membre['company']."</td>
+                        <td>".$membre['name']."</td>
+                        <td><a href='' class='button'></a></td>                
+                    </tr>";
+            }      
+        ?>
+    </table>
 </div>
 
