a99c9c82b647c437f3f503084db3eef644bbb6d6
Olivier Raude <olivierraude@gmail.com>
2020-02-28 08:11:28 -0500

    installation du plugIn message

---
+++ app_photo/language/english/mahana_lang.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Account Creation
+$lang['mahana_'.MSG_SUCCESS] 	  	             = 'Succès';
+$lang['mahana_'.MSG_ERR_GENERAL] 	  	         = 'Erreur';
+$lang['mahana_'.MSG_ERR_INVALID_USER_ID] 	  	 = 'Id usager non specifié';
+$lang['mahana_'.MSG_ERR_INVALID_MSG_ID] 	  	 = 'Le message n\'existe pas';
+$lang['mahana_'.MSG_ERR_INVALID_THREAD_ID] 	  	 = 'Le message n\'existe pas dans ce fil';
+$lang['mahana_'.MSG_ERR_INVALID_STATUS_ID] 	  	 = 'Statut non specifié';
+$lang['mahana_'.MSG_ERR_INVALID_SENDER_ID] 	  	 = 'Expéditeur npn valide';
+$lang['mahana_'.MSG_ERR_INVALID_RECIPIENTS] 	 = 'Ce destinataire n\'existe pas';
+$lang['mahana_'.MSG_MESSAGE_SENT] 	  	         = 'Message envoyé';
+$lang['mahana_'.MSG_STATUS_UPDATE] 	  	         = 'Statut mis à jour';
+$lang['mahana_'.MSG_PARTICIPANT_ADDED] 	  	     = 'Participant ajouté';
+$lang['mahana_'.MSG_ERR_PARTICIPANT_EXISTS] 	 = 'L\'usager participe déjà à ce fil';
+$lang['mahana_'.MSG_ERR_PARTICIPANT_NONSYSTEM] 	 = 'Cet usager n\'est pas enregistré';
+$lang['mahana_'.MSG_PARTICIPANT_REMOVED] 	     = 'Participant retiré du fil';
--- fichiers_projet/database/db_picturing.sql
+++ fichiers_projet/database/db_picturing.sql
@@ -290,6 +290,60 @@ INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
 (13, 5, 6),
 (14, 7, 3);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `msg_messages`
+--
+
+CREATE TABLE `msg_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `thread_id` int(11) NOT NULL,
+  `body` text NOT NULL,
+  `priority` int(2) NOT NULL DEFAULT '0',
+  `sender_id` int(11) NOT NULL,
+  `cdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `msg_participants`
+--
+
+CREATE TABLE `msg_participants` (
+  `user_id` int(11) NOT NULL,
+  `thread_id` int(11) NOT NULL,
+  `cdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`,`thread_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `msg_status`
+--
+
+CREATE TABLE `msg_status` (
+  `message_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` int(2) NOT NULL,
+  PRIMARY KEY (`message_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `msg_threads`
+--
+
+CREATE TABLE `msg_threads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
 --
 -- Contraintes pour les tables déchargées
 --
