c43eade1a097944d4bd7e9ee2918e672f0c07204
Salhi <e1895293@cmaisonneuve.qc.ca>
2020-03-02 14:39:36 -0500

    scss apropo de nous

--- app_photo/views/pages/a_propos.php
+++ app_photo/views/pages/a_propos.php
@@ -1,35 +1,40 @@
-<div>
-    <h3> A propos </h3>
+<div class="apropos">
+    <h3> A propos de nous</h3>
 
     <p>
-        <h4>
+        
                 Depuis 1981, nous offrons notre plateforme pour vendre, 
             acheter ou échanger les meilleurs appareils,
             objectifs, accessoires, mais surout le meilleur service pour 
             tout ce qui a trait à la photographie à Montréal.
-        </h4>
+        
     </p>
-    <p>
-        <h4>
+    
                 Avec nos deux succursales (26 Notre-Dame E & 495 McGill), 
             nous sommes idéalement situés pour vous servir.
-        </h4>
+        
     </p>
     <p>
-        <h4>
-                Nous offrons également une vaste gamme de services de laboratoire
+        
+            Nous offrons également une vaste gamme de services de laboratoire
             professionnels. 
             Passez-nous voir ou appelez nous !
-        </h4>
+        
     </p>
     <p>
-        <h4>
-                 vue a proposEn tant que revendeur agréé, nous disposons d'un grand choix dans 
+        
+            vue a proposEn tant que revendeur agréé, nous disposons d'un grand choix dans 
             toutes les grandes marques de matériel photographique, telles Leica, 
             Zeiss, Nikon, Canon, Olympus, Fujifilm, etc. Allant du petit compact au 
             ssionnel, en passant par les trépieds, 
             les sacs et autres accessoires, vous trouverez tout chez nous ! 
-        </h4>
+        
     </p>
+    <p>
+        
+    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2793.9883658423!2d-73.54511998411432!3d45.55055957910206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91c761d266345%3A0xafb3d6180daabe53!2s2050%20Boulevard%20Pie-IX%2C%20Montr%C3%A9al%2C%20QC%20H1V%203T6!5e0!3m2!1sfr!2sca!4v1583177551486!5m2!1sfr!2sca" width="700" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    </p>
+    
+</p>
 
 </div>
