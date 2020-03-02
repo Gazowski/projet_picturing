a85388c2536f970bb65637989af7d7e53e83f860
Salhi <e1895293@cmaisonneuve.qc.ca>
2020-03-02 15:06:37 -0500

    Maj scss

--- app_photo/views/pages/a_propos.php
+++ app_photo/views/pages/a_propos.php
@@ -1,5 +1,5 @@
 <div class="apropos">
-    <h3> A propos de nous</h3>
+    <h2> A propos de nous</h2>
 
     <p>
         
@@ -28,12 +28,12 @@
             Zeiss, Nikon, Canon, Olympus, Fujifilm, etc. Allant du petit compact au 
             ssionnel, en passant par les trépieds, 
             les sacs et autres accessoires, vous trouverez tout chez nous ! 
-        
+         
     </p>
-    <p>
-        
-    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2793.9883658423!2d-73.54511998411432!3d45.55055957910206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91c761d266345%3A0xafb3d6180daabe53!2s2050%20Boulevard%20Pie-IX%2C%20Montr%C3%A9al%2C%20QC%20H1V%203T6!5e0!3m2!1sfr!2sca!4v1583177551486!5m2!1sfr!2sca" width="700" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-    </p>
+    
+    <h2>Localisation</h2>    
+    <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2793.9883658423!2d-73.54511998411432!3d45.55055957910206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91c761d266345%3A0xafb3d6180daabe53!2s2050%20Boulevard%20Pie-IX%2C%20Montr%C3%A9al%2C%20QC%20H1V%203T6!5e0!3m2!1sfr!2sca!4v1583177551486!5m2!1sfr!2sca"  frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    </div>
     
 </p>
 
