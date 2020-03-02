b7db9c9b4edf284e41179cad1333327ab9d47261
bob <bob@>
2020-03-02 11:01:21 -0500

    maj vue a propos

--- app_photo/views/pages/a_propos.php
+++ app_photo/views/pages/a_propos.php
@@ -3,7 +3,7 @@
 
     <p>
         <h4>
-            Depuis 1981, nous offrons notre plateforme pour vendre, 
+                Depuis 1981, nous offrons notre plateforme pour vendre, 
             acheter ou échanger les meilleurs appareils,
             objectifs, accessoires, mais surout le meilleur service pour 
             tout ce qui a trait à la photographie à Montréal.
@@ -11,20 +11,20 @@
     </p>
     <p>
         <h4>
-            Avec nos deux succursales (26 Notre-Dame E & 495 McGill), 
+                Avec nos deux succursales (26 Notre-Dame E & 495 McGill), 
             nous sommes idéalement situés pour vous servir.
         </h4>
     </p>
     <p>
         <h4>
-            Nous offrons également une vaste gamme de services de laboratoire
+                Nous offrons également une vaste gamme de services de laboratoire
             professionnels. 
             Passez-nous voir ou appelez nous !
         </h4>
     </p>
     <p>
         <h4>
-            En tant que revendeur agréé, nous disposons d'un grand choix dans 
+                 vue a proposEn tant que revendeur agréé, nous disposons d'un grand choix dans 
             toutes les grandes marques de matériel photographique, telles Leica, 
             Zeiss, Nikon, Canon, Olympus, Fujifilm, etc. Allant du petit compact au 
             ssionnel, en passant par les trépieds, 
