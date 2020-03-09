e2941d92c41259bb83a1b09ee81dd98a805fb64b
Comeau <e1895410@cmaisonneuve.qc.ca>
2020-03-09 09:08:39 -0400

    my_loader.php: verif de is_IE si existant

--- app_photo/core/MY_Loader.php
+++ app_photo/core/MY_Loader.php
@@ -45,7 +45,8 @@ class MY_Loader extends CI_Loader {
         $vars['filter'] =  (strpos($page, 'list') || strpos($page,'tile')) ? $filter_view : '';
 
         // si le navigateur est IE
-        $vars['is_IE'] = $_SESSION['is_IE'] ? '-ie-' : ''; 
+        if(isset($_SESSION['is_IE']))
+            $vars['is_IE'] = $_SESSION['is_IE'] ? '-ie-' : ''; 
 
         $this->view('pages/head', $vars);
         $this->view($header, $vars);
